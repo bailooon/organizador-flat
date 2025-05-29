812643306547c80e5dabab10d90362457e38902e
bailooon <matheus@bailon.com.br>
2025-05-29 17:13:53 -0300

    adicionando nav e routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TarefaComponent } from './components/tarefa/tarefa.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TarefaComponent],
+  imports: [RouterOutlet, TarefaComponent,HomeComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TarefaComponent } from './components/tarefa/tarefa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'tarefas', component: TarefaComponent },
+];
